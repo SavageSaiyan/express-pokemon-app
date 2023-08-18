9d22d5383ea0849037a633a5b877b3c2d3d6f148
Sterling <sterlingduncan9@gmail.com>
2023-08-18 14:38:38 -0400

    All my npm packages are added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to the Pokemon App!');
+})
+
+
+app.listen(3000, (req, res)=>{
+    console.log('listening')
+})
